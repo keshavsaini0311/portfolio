2c1c8b78effa15b846d5e16dd7164bfa0c377894
keshavsaini0311 <keshavsaini1234567890@gmail.com>
2024-05-28 15:26:18 +0530

    UI changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,23 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { FaGithub ,FaLinkedinIn} from 'react-icons/fa'
 
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white p-4">
-      <div className="container mx-auto text-center">
-        &copy; {new Date().getFullYear()} My Portfolio. All rights reserved.
+      <div className="container flex justify-center text-center">
+      <h1 className="text-white ">
+        &copy; {new Date().getFullYear()+" "} 
+          
+          My Portfolio. All rights reserved.
+        </h1>
+        <a href="https://github.com/keshavsaini0311" className="text-white  hover:text-gray-400 ml-4">
+          <FaGithub size={30} />
+        </a>
+        <a href="https://www.linkedin.com/in/keshav-saini-190a53256/" className=" text-white hover:text-gray-400 ml-4">
+          <FaLinkedinIn size={30} />
+        </a>
       </div>
     </footer>
   )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,7 +15,7 @@ export default function Projects() {
           <h1 className="text-3xl text-white text-center font-bold mb-4">Projects</h1>
           <div className="text-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
             {projectList.map((project, index) => (
-              <div key={index} className="bg-zinc-800 p-4 rounded shadow-lg transform transition hover:scale-105 h-fit w-fit mx-auto">
+              <div key={index} className="bg-zinc-800 p-3 rounded shadow-lg transform transition hover:scale-105 h-fit w-fit mx-auto">
                 <img src={project.image} alt={project.name} className="" />
                 <h2 className="text-xl text-center font-bold">{project.name}</h2>
                 <p>{project.description}</p>
